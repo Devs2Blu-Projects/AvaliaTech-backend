a39eeaf307750667ec72d2be6bd7155e190bb766
raphael-teodoro <raphaelcteodoro@hotmail.com>
2023-10-04 19:25:04 -0300

    Added UserModel.Course.

--- hackweek-backend/Models/UserModel.cs
+++ hackweek-backend/Models/UserModel.cs
@@ -7,5 +7,6 @@
         public string Name { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public string Course { get; set; } = string.Empty;
     }
 }
