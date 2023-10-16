dda3c8a234a6e2214e21f32530d44c32888943a5
raphael-teodoro <raphaelcteodoro@hotmail.com>
2023-10-15 22:47:22 -0300

    fix: Removed constant "api/" from endpoints.

--- hackweek-backend/Controllers/LoginController.cs
+++ hackweek-backend/Controllers/LoginController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace hackweek_backend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class LoginController : ControllerBase
     {
